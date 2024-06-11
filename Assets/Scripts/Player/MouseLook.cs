408cd251eefe35e7de97d01f7c37cfa8ffa52059
Konrad <kondykox@gmail.com>
2024-06-11 17:58:58 +0200

    new player movement

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float sensitivity = 300f;
+    public float mouseSensitivity = 100f;
     public Transform playerBody;
 
     float xRotation = 0f;
@@ -10,14 +10,14 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,93 +2,82 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController characterController;
+    public CharacterController controller;
+    public GameObject player;
 
-    [Header("Player settings")]
-    public float speed = 15f;
+    [Header("Speed")]
+    public float speed = 12;
+
+    [Header("Gravity")]
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
-    public float crouchScale = 0.75f;
-    float originalHeight;
-    Vector3 normalScale;
-
-    [Header("Ground")]
+    [Header("Ground Check")]
     public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask whatIsGround;
+    public LayerMask groundMask;
 
+    // Crouching variables
+    float standingHeight;
+    float crouchingHeight;
     Vector3 velocity;
     bool isGrounded;
 
     void Start()
     {
-        originalHeight = characterController.height;
-        normalScale = transform.localScale;
+        standingHeight = player.transform.localScale.y;
+        crouchingHeight = standingHeight * 0.7f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckGround();
-        Movement();
-        ApplyGravity();
-
-        // Crouching
-        if (Input.GetKey(KeyCode.LeftControl))
-            StartCrouch();
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-            StopCrouch();
-    }
-
-    #region CheckGround
-    // Check if player is on the ground
-    void CheckGround()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
             velocity.y = -2f;
-    }
-    #endregion
 
-    #region Player movement
-    void Movement()
-    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        characterController.Move(move * speed * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
 
+        // Jump
         if (Input.GetButtonDown("Jump") && isGrounded)
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-    }
-    #endregion
+            Jump();
 
-    #region Player gravity
-    void ApplyGravity()
-    {
+        // Apply crouching if pressing crouch button
+        if (Input.GetButtonDown("Crouch"))
+            Crouch();
+        if (Input.GetButtonUp("Crouch"))
+            StandUp();
+
+        // Gravity
         velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
-    }
-    #endregion
-
-    #region Crouching
-    void StartCrouch()
-    {
-        characterController.height = crouchScale;
-        transform.localScale = new Vector3(transform.localScale.x, crouchScale / normalScale.y, transform.localScale.z);
-        if (isGrounded)
-            characterController.Move(Vector3.down * ((originalHeight - crouchScale) / 2));
+        controller.Move(velocity * Time.deltaTime);
     }
 
-    void StopCrouch()
+    void Jump()
     {
-        characterController.height = originalHeight;
-        transform.localScale = normalScale;
-        if (isGrounded)
-            characterController.Move(Vector3.up * ((crouchScale - originalHeight) / 2));
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
     }
-    #endregion
+
+    void Crouch()
+    {
+        player.transform.localScale = new Vector3(
+            player.transform.localScale.x,
+            crouchingHeight,
+            player.transform.localScale.z
+        );
+    }
+
+    void StandUp()
+    {
+        player.transform.localScale = new Vector3(
+            player.transform.localScale.x,
+            standingHeight,
+            player.transform.localScale.z
+        );
+    }
 }
